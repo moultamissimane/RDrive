a045dc9ffed2f46277dd534a9290f0b5bff5e1db
Moultamiss Imane <93978575+moultamissimane@users.noreply.github.com>
2023-09-19 13:52:18 +0100

    edit tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,47 +1,54 @@
+const { nextui } = require('@nextui-org/react');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    fontSize: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
+  mode: 'jit',
+  content: ['./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}','./src/**/*.{js,ts,jsx,tsx}'],
+  serverless: true,
+  serverlessConfig: {
+    api: {
+      timeout: 30,
     },
+  },
+  theme: {
     extend: {
       colors: {
-        primary: {
-          100: '#E6F6FE',
-          200: '#C0EAFC',
-          300: '#9ADDFB',
-          400: '#4FC3F7',
-          500: '#03A9F4',
-          600: '#0398DC',
-          700: '#026592',
-          800: '#014C6E',
-          900: '#013349',
-        },
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.zinc,
+        red: colors.rose,
+        yellow: colors.amber,
+        green: colors.green,
+        blue: colors.sky,
+        indigo: colors.indigo,
+        purple: colors.purple,
+        pink: colors.pink,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        orange: colors.orange,
+      },
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
         gray: {
-          100: '#f7fafc',
-          200: '#edf2f7',
-          300: '#e2e8f0',
-          400: '#cbd5e0',
-          500: '#a0aec0',
-          600: '#718096',
-          700: '#4a5568',
-          800: '#2d3748',
-          900: '#1a202c',
+          850: '#222226',
+          666: '#666',
+        },
+        slate: {
+          900: '#000000',
+          800: '#1e293b',
+          700: '#334155',
         },
       },
-      lineHeight: {
-        hero: '4.5rem',
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      },
+      flexGrow: {
+        'half': 0.5,
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
